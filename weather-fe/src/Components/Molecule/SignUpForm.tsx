d74e50581a0dba8add64c03277b172f3ba010e13
Danila <danila.pekhtserau@gmail.com>
2022-12-18 16:38:49 +0300

    FE-add molecule SignUpForm

---
+++ weather-fe/src/Components/Molecule/SignUpForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AInput from "../Atoms/AInput";
+import AButton from "../Atoms/AButton";
+
+const SignUpForm = () => {
+    return (
+        <form>
+            <AInput placeholder={"email"}/>
+            <AInput placeholder={"password"} type={"password"}/>
+            <AButton text={"Sign Up"}/>
+        </form>
+    );
+};
+
+export default SignUpForm;
