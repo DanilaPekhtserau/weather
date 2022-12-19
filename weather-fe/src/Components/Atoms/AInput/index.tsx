34cd14755cabc74a9d1653282324b1ad1551c8f0
Danila <danila.pekhtserau@gmail.com>
2022-12-19 14:46:00 +0300

    FE-add OnChange prop to AInput

--- weather-fe/src/Components/Atoms/AInput/index.tsx
+++ weather-fe/src/Components/Atoms/AInput/index.tsx
@@ -3,11 +3,13 @@ import React, {FC} from 'react';
 interface AInputProps {
     type?: React.HTMLInputTypeAttribute
     placeholder?: string
+
+    onChange?: () => void
 }
 
-const AInput: FC<AInputProps> = ({placeholder, type}) => {
+const AInput: FC<AInputProps> = ({placeholder, type, onChange}) => {
     return (
-        <input placeholder={placeholder} type = {type}>
+        <input placeholder={placeholder} type = {type} onChange={() => {onChange && onChange()}}>
             
         </input>
     );
