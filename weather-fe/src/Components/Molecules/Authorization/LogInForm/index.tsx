ebc36bf1669eb7fe8c2ce641b93f2981e1fb1fb3
Danila <danila.pekhtserau@gmail.com>
2022-12-20 13:13:22 +0300

    FE-add LogInForm

---
+++ weather-fe/src/Components/Molecules/Authorization/LogInForm/index.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import AInput from "../../../Atoms/AInput";
+import AButton from "../../../Atoms/AButton";
+import {sign_in_user} from "../../../RESTAPI/BackendAPI";
+
+const LogInForm = () => {
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const log_in_user = () => {
+        const data = {
+            user: {
+                email: email,
+                password: password
+            }
+        }
+        sign_in_user(data).then(r=>
+        {
+            alert(r)
+            return
+        })
+    }
+    return (
+        <form onSubmit={e => {
+            e.preventDefault()
+        }}>
+            <AInput placeholder={"email"} onChange={(e) => setEmail(e.target.value)}/>
+            <AInput placeholder={"password"} type={"password"} onChange={(e) => setPassword(e.target.value)}/>
+            <AButton text={"Log In"}
+                     func={() => log_in_user()}/>
+        </form>
+    );
+};
+
+export default LogInForm;
--- weather-fe/src/Components/RESTAPI/BackendAPI.ts
+++ weather-fe/src/Components/RESTAPI/BackendAPI.ts
@@ -6,3 +6,7 @@ const backendURL = "http://localhost:3000/"
 export const sign_up_user=(data:any)=>{
     return axios.post(`${backendURL}users`, data)
 }
+
+export const sign_in_user=(data:any)=>{
+    return axios.get(`${backendURL}users/sign_in`, data)
+}
