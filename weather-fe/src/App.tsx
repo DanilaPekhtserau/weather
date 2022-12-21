6fda91ebcbd5999ddf7b4d59b01623231bcf3fbd
Danila <danila.pekhtserau@gmail.com>
2022-12-21 13:45:24 +0300

    FE-add lib react-router-dom

--- weather-fe/src/App.tsx
+++ weather-fe/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import SignUpForm from "./Components/Molecules/Authorization/SignUpForm";
+import LogInForm from "./Components/Molecules/Authorization/LogInForm";
 
 function App() {
-  return (
-    <div className="App">
-      <SignUpForm/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SignUpForm/>
+            -------------
+            <LogInForm/>
+        </div>
+    );
 }
 
 export default App;
--- weather-fe/src/index.tsx
+++ weather-fe/src/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import SignUpForm from "./Components/Molecules/Authorization/SignUpForm";
+import LogInForm from "./Components/Molecules/Authorization/LogInForm";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: "/log-in",
+        element: <LogInForm/>,
+    },
+    {
+        path: "/sign-up",
+        element: <SignUpForm/>,
+    },
+]);
+
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
+
